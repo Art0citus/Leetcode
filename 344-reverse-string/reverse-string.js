4eeb8a5b26ebe94d73b0f6e6a6104f6cbc96a640
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-01-12 20:48:04 +0530

    Time: 1 ms (56.21%) | Memory: 61 MB (56.39%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,11 +3,15 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    let left = 0;
+         let left = 0;
     let right = s.length - 1;
 
-    while (left < right){
-        [s[left], s[right]] = [s[right], s[left]];
+    while (left < right) {
+        // Swap
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
         left++;
         right--;
     }
