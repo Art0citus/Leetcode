302beeebfeacd1a492be684fa064812bc45d84c0
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-01-13 19:00:53 +0530

    Time: 3 ms (95.61%) | Memory: 61.8 MB (29.79%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const arr = s.split('');
+    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        if (!vowels.has(arr[left])) {
+            left++;
+        } else if (!vowels.has(arr[right])) {
+            right--;
+        } else {
+            // both are vowels → swap
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+
+    return arr.join('');
+};
