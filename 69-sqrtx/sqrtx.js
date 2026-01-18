96cda1a9acc8d856510fc92d115c4ac0fa46581f
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-01-18 23:07:59 +0530

    Time: 1 ms (71.61%) | Memory: 55.8 MB (53.66%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+     if (x < 2) return x;
+
+    let left = 1;
+    let right = Math.floor(x / 2);
+    let ans = 0;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (mid * mid === x) {
+            return mid;
+        }
+
+        if (mid * mid < x) {
+            ans = mid;        // store best candidate
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return ans;
+};
