63f404f9e4623a7ae10d16139d03a49c6442f7da
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-01-23 20:41:15 +0530

    Time: 3 ms (89.77%) | Memory: 60.8 MB (64.41%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,28 +3,28 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    const values = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+     const map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
     };
 
     let total = 0;
-    for (let i = 0; i < s.length; i++) {
-        let curr = values[s[i]];
-        let next = values[s[i + 1]];
 
-        // if current is smaller than next, subtract (like IV, IX, etc.)
+    for (let i = 0; i < s.length; i++) {
+        const curr = map[s[i]];
+        const next = map[s[i + 1]];
+
         if (next && curr < next) {
             total -= curr;
         } else {
             total += curr;
         }
     }
-    return total;
 
+    return total;
 };
