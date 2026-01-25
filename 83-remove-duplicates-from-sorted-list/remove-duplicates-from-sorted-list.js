de89cbb89e7126c2d439700ae0bef5c56a0890e6
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-01-25 20:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (65.00%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -11,14 +11,12 @@
  */
 var deleteDuplicates = function(head) {
      let current = head;
-
     while (current && current.next) {
         if (current.val === current.next.val) {
-            current.next = current.next.next; // remove duplicate
+            current.next = current.next.next; 
         } else {
             current = current.next;
         }
     }
-
     return head;
 };
