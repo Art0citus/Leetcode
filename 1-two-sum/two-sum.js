9f3e192c92bfe9e8352231e0b18242fcaf45d293
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-02-03 23:16:24 +0530

    Time: 3 ms (59.16%) | Memory: 55.9 MB (31.08%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,24 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
- const arr = nums.map((num, idx) => [num, idx]);
-    
-    arr.sort((a, b) => a[0] - b[0]);
-    
-    let left = 0;
-    let right = arr.length - 1;
-    
-    while (left < right) {
-        const sum = arr[left][0] + arr[right][0];
-        
-        if (sum === target) {
-            return [arr[left][1], arr[right][1]];
-        } else if (sum < target) {
-            left++;
-        } else {
-            right--;
+ const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
         }
+
+        map.set(nums[i], i);
     }
-    
-    return [];
 };
