ab9e86a2eab6680ab197c8f94998c1e161eb9adb
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-02-25 22:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (29.97%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -4,13 +4,18 @@
  */
 var lengthOfLastWord = function(s) {
     let length = 0;
-  let i = s.length - 1;
-  while (i >= 0 && s[i] === ' ') {
-    i--;
-  }
-  while (i >= 0 && s[i] !== ' ') {
-    length++;
-    i--;
-  }
-  return length;
+    let i = s.length - 1;
+
+    // Phase 1: Skip trailing spaces
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+
+    // Phase 2: Count last word length
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
 };
