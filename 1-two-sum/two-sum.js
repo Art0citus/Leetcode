3d520bb4eeabfd42d4a33ceab3d210ab9313b35f
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-02-28 21:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (35.86%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map  = new Map();
+   const map = new Map(); // number → index
 
-    for (let i = 0; i<nums.length; i++) {
-        let complement = target - nums[i];
+    for (let i = 0; i < nums.length; i++) {
 
-        if (map.has(complement)) {
-            return [map.get(complement), i];
+        const needed = target - nums[i];
+
+        if (map.has(needed)) {
+            return [map.get(needed), i];
         }
-        map.set(nums[i],i);
+
+        map.set(nums[i], i);
     }
 };
