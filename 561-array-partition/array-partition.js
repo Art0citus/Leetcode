db7e804151bab279ca12cd952f9fb8cfcfaba21d
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-03-02 20:59:11 +0530

    Time: 36 ms (87.85%) | Memory: 60.4 MB (77.89%) - LeetSync

--- 561-array-partition/array-partition.js
+++ 561-array-partition/array-partition.js
@@ -6,10 +6,9 @@ var arrayPairSum = function(nums) {
     nums.sort((a, b) => a - b);
 
     let sum = 0;
+
     for (let i = 0; i < nums.length; i += 2) {
         sum += nums[i];
     }
-
     return sum;
-    
 };
