3ed97fad4cc9623b6eddaab44ea79d3419bb1d2b
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-03-11 20:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (28.55%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,17 +3,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 0) return "";
+    
+    let prefix = strs[0];
 
-    for (let i = 0; i < strs[0].length; i++) {
-        let char = strs[0][i];
+    for (let i = 1; i < strs.length; i++) {
 
-        for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== char) {
-                return strs[0].substring(0, i);
-            }
+        while (!strs[i].startsWith(prefix)) {
+            prefix = prefix.slice(0, prefix.length - 1);
+
+            if (prefix === "") return "";
         }
     }
 
-    return strs[0];
+    return prefix;
 };
