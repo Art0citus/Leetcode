4553f7e3bb7dbdc9270d5d188f7fcc6e29cfcf7b
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-03-20 21:28:57 +0530

    Time: 40 ms (98.44%) | Memory: 59.3 MB (56.92%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,17 +11,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let slow = head;
+   let slow = head;
     let fast = head;
 
     while (fast !== null && fast.next !== null) {
-        slow = slow.next;
-        fast = fast.next.next;
+        slow = slow.next;          // move 1 step
+        fast = fast.next.next;     // move 2 steps
 
         if (slow === fast) {
-            return true;
+            return true;           // cycle detected
         }
     }
 
-    return false;
+    return false;                  // no cycle
 };
