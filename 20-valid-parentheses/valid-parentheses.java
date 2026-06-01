2c37b50ee931824ac5332eec2d8c475099b8caf4
Ritik Mishra <147702864+Art0citus@users.noreply.github.com>
2026-06-01 21:36:28 +0530

    Time: 3 ms (88.11%) | Memory: 42.8 MB (95.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for (char ch : s.toCharArray()) {
-
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             }
             else {
-
                 if (stack.isEmpty()) {
                     return false;
                 }
-
                 char top = stack.pop();
-
                 if ((ch == ')' && top != '(') ||
                     (ch == '}' && top != '{') ||
                     (ch == ']' && top != '[')) {
@@ -22,7 +17,6 @@ class Solution {
                 }
             }
         }
-
         return stack.isEmpty();
     }
 }
